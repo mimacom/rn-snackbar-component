3f1e5c2701c34f7ace31bb8126f452be2b0dda4b
Jesús Miguel Cruz Cana <jesus.cruz@mimacom.com>
2025-03-28 09:32:49 +0100

    Fixed defaultProps deprecation warning.

--- src/components/Tap.js
+++ src/components/Tap.js
@@ -1,37 +1,31 @@
-import React from 'react'
-import {
-  View,
-  TouchableOpacity,
-  TouchableNativeFeedback,
-} from 'react-native'
+import React from "react";
+import { View, TouchableOpacity, TouchableNativeFeedback } from "react-native";
 
-import {
-  IS_ANDROID,
-  IS_LT_LOLLIPOP,
-} from '../utils'
-import { TapDefaultProps, TapProps } from '../common/properties'
+import { IS_ANDROID, IS_LT_LOLLIPOP } from "../utils";
+import { TapDefaultProps, TapProps } from "../common/properties";
 
-const Tap = ({ onPress, style, children }) => {
+const Tap = ({
+  children = TapDefaultProps.children,
+  style = TapDefaultProps.style,
+  onPress = TapDefaultProps.onPress,
+}) => {
   if (IS_ANDROID && !IS_LT_LOLLIPOP) {
     return (
       <TouchableNativeFeedback
         background={TouchableNativeFeedback.SelectableBackgroundBorderless()}
         onPress={onPress}
       >
-        <View style={style}>
-          {children}
-        </View>
+        <View style={style}>{children}</View>
       </TouchableNativeFeedback>
-    )
+    );
   }
   return (
     <TouchableOpacity onPress={onPress} style={style}>
       {children}
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-Tap.propTypes = TapProps
-Tap.defaultProps = TapDefaultProps
+Tap.propTypes = TapProps;
 
-export default Tap
+export default Tap;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React, { Component, Fragment } from 'react'
-import { Text, Animated, Easing } from 'react-native'
+import React, { Component, Fragment } from "react";
+import { Text, Animated, Easing } from "react-native";
 
-import Tap from './components/Tap'
-import styles from './common/style'
-import { SnackbarProps, SnackbarDefaultProps } from './common/properties'
+import Tap from "./components/Tap";
+import styles from "./common/style";
+import { SnackbarProps, SnackbarDefaultProps } from "./common/properties";
 
 /*
  * Values are from https://material.io/guidelines/motion/duration-easing.html#duration-easing-dynamic-durations
@@ -11,35 +11,37 @@ import { SnackbarProps, SnackbarDefaultProps } from './common/properties'
 const easingValues = {
   entry: Easing.bezier(0.0, 0.0, 0.2, 1),
   exit: Easing.bezier(0.4, 0.0, 1, 1),
-}
+};
 
 const durationValues = {
   entry: 225,
   exit: 195,
-}
+};
 
 class Snackbar extends Component {
+  static defaultProps = SnackbarDefaultProps;
+
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       translateValue: new Animated.Value(0),
       hideDistance: 9999,
-    }
-    this.handleLayoutChange = this.handleLayoutChange.bind(this)
-    this.hideSnackbar = this.hideSnackbar.bind(this)
+    };
+    this.handleLayoutChange = this.handleLayoutChange.bind(this);
+    this.hideSnackbar = this.hideSnackbar.bind(this);
   }
 
   handleLayoutChange(event) {
-    this.setState({ hideDistance: event.nativeEvent.layout.height })
+    this.setState({ hideDistance: event.nativeEvent.layout.height });
   }
 
   componentDidMount() {
-    this.state.translateValue.setValue(this.props.visible ? 1 : 0)
+    this.state.translateValue.setValue(this.props.visible ? 1 : 0);
   }
 
   // eslint-disable-next-line camelcase
   UNSAFE_componentWillReceiveProps(nextProps) {
-    const { visible, native } = this.props
+    const { visible, native } = this.props;
 
     if (nextProps.visible && !visible) {
       Animated.timing(this.state.translateValue, {
@@ -47,26 +49,28 @@ class Snackbar extends Component {
         toValue: 1,
         easing: easingValues.entry,
         useNativeDriver: native,
-      }).start()
+      }).start();
       if (nextProps.autoHidingTime) {
-        const hideFunc = this.hideSnackbar.bind(this)
-        setTimeout(hideFunc, nextProps.autoHidingTime)
+        const hideFunc = this.hideSnackbar.bind(this);
+        setTimeout(hideFunc, nextProps.autoHidingTime);
       }
     } else if (!nextProps.visible && visible) {
-      this.hideSnackbar()
+      this.hideSnackbar();
     }
   }
 
   // eslint-disable-next-line camelcase
   UNSAFE_componentWillUpdate(nextProps, nextState) {
-    const { visible, distanceCallback, bottom } = this.props
+    const { visible, distanceCallback, bottom } = this.props;
 
-    if (nextProps.visible !== visible
-      || nextState.hideDistance !== this.state.hideDistance) {
+    if (
+      nextProps.visible !== visible ||
+      nextState.hideDistance !== this.state.hideDistance
+    ) {
       if (nextProps.visible) {
-        distanceCallback(nextState.hideDistance + bottom)
+        distanceCallback(nextState.hideDistance + bottom);
       } else {
-        distanceCallback(bottom)
+        distanceCallback(bottom);
       }
     }
   }
@@ -74,22 +78,27 @@ class Snackbar extends Component {
   /**
    * Starting te animation to hide the snack bar.
    * @return {null} No return.
-  */
+   */
   hideSnackbar() {
     Animated.timing(this.state.translateValue, {
       duration: durationValues.exit,
       toValue: 0,
       easing: easingValues.exit,
       useNativeDriver: this.props.native,
-    }).start()
+    }).start();
   }
 
   render() {
     const {
-      position, message, messageStyle, containerStyle,
-      action, actionHandler, actionContainerStyle,
+      position,
+      message,
+      messageStyle,
+      containerStyle,
+      action,
+      actionHandler,
+      actionContainerStyle,
       actionTextStyle,
-    } = this.props
+    } = this.props;
 
     return (
       <Animated.View
@@ -118,13 +127,9 @@ class Snackbar extends Component {
           onLayout={this.handleLayoutChange}
         >
           {String(message) === message ? (
-            <Text style={[styles.text_msg, messageStyle]}>
-              {message}
-            </Text>
+            <Text style={[styles.text_msg, messageStyle]}>{message}</Text>
           ) : (
-            <Fragment>
-              {message}
-            </Fragment>
+            <Fragment>{message}</Fragment>
           )}
           {actionHandler && !!action ? (
             <Tap
@@ -136,19 +141,16 @@ class Snackbar extends Component {
                   {action.toUpperCase()}
                 </Text>
               ) : (
-                <Fragment>
-                  {action}
-                </Fragment>
+                <Fragment>{action}</Fragment>
               )}
             </Tap>
           ) : null}
         </Animated.View>
       </Animated.View>
-    )
+    );
   }
 }
 
-Snackbar.defaultProps = SnackbarDefaultProps
-Snackbar.propTypes = SnackbarProps
+Snackbar.propTypes = SnackbarProps;
 
-export default Snackbar
+export default Snackbar;
